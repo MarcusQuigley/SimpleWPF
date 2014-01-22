f0c1b73ca08894cf026effff073b59b020db452e
marcusquigley <marcusquigley@gmail.com>
2014-01-22 13:26:18 -0500

    Adding data Project

---
+++ SampleData/Processes.cs
@@ -0,0 +1,73 @@
+﻿ 
+namespace SimpleWPF.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading;
+    using System.ComponentModel;
+
+    public delegate void ProcessesHandler(List<Process> processes);
+
+    public class Processes
+    {
+        private TimeSpan sleepDuration;
+        Thread workerThread;
+        private bool _endProcess = false;
+        public event ProcessesHandler ProcessList = delegate { };
+
+        public bool EndProcess
+        {
+            get { return _endProcess; }
+            set
+            {
+                _endProcess = value;
+                if (value == true)
+                {
+                    workerThread.Join();
+                    //      Console.WriteLine("Thread has ended");
+                }
+            }
+        }
+
+        public Processes(TimeSpan duration)
+        {
+            sleepDuration = duration;
+
+            workerThread = new Thread(DoWork);
+            workerThread.Start();
+        }
+
+        public Processes(double durationInMilliseconds)
+            : this(TimeSpan.FromMilliseconds(durationInMilliseconds)) { }
+
+
+        protected void RaiseProcesses(List<Process> processes)
+        {
+            ProcessesHandler handler = ProcessList;
+            if (handler != null)
+            {
+                handler(processes);
+            }
+        }
+
+
+        void DoWork()
+        {
+            while (EndProcess == false)
+            {
+                var list = Process.GetProcesses().ToList();
+                RaiseProcesses(list);
+                Thread.Sleep(sleepDuration);
+            }
+
+            //  Console.WriteLine("Ending thread");
+        }
+
+        //~Processes()
+        //{
+        //    Console.WriteLine("IN finalize of GetProcess");
+        // }
+    }
+}
---
+++ SampleData/Program.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace SampleData
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Testing GetProcess");
+
+//            GetProcesses process = new GetProcesses(1000);
+//            process.ProcessList +=  process_ProcessList;
+
+//            Console.WriteLine("Enter to quit");
+
+
+
+//            while (true)
+//            {
+//                ConsoleKeyInfo key = Console.ReadKey();
+//                if (key.Key == ConsoleKey.Enter)
+//                {
+//                    process.EndProcess = true;
+//                    break;
+//                }
+//                else
+//                {
+//                    Console.WriteLine(key.KeyChar);
+//                }
+//            }
+
+//            Console.WriteLine("About to quit");
+//          //  process.ProcessCount -= process_ProcessCount;
+//             process = null;
+//             GC.Collect();
+//             GC.WaitForPendingFinalizers();
+//             GC.Collect();
+//            System.Threading.Thread.Sleep(2000);
+
+
+            
+//        }
+
+//        static void process_ProcessList(List<System.Diagnostics.Process> processes)
+//        {
+//            Console.WriteLine("Process Count : {0}", processes.Count);
+//        }
+
+//        static void process_ProcessCount(int count)
+//        {
+//            Console.WriteLine("Process Count : {0}", count);
+//        }
+//    }
+//}
